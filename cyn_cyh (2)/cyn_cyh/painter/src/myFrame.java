accf6b323c77b3611f8c0bed23a0a1cc38bd6e59
Serchen <15941797526@163.com>
2020-12-02 15:05:39 +0800

    12/2

--- cyn_cyh (2)/cyn_cyh/painter/src/myFrame.java
+++ cyn_cyh (2)/cyn_cyh/painter/src/myFrame.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
@@ -10,10 +12,18 @@ public class myFrame extends JFrame {
     }
     //声明一个用来存储形状的数组
     public static ArrayList<shape> shapes = new ArrayList<>();
+    //
+    public static shape shapes_temp = null;
     //声明监听器
     public DrawListener listener = new DrawListener();
     //
-    public static JPanel jp1 = new JPanel(new GridLayout(8,2,10,10));
+    public Color frontcolor = Color.BLUE;
+
+    public int width;
+
+    public Color backgroundcolor;
+    //
+    public static JPanel jp1 = new JPanel(new GridLayout(15,2,10,10));
     //
     public static JPanel jp2 = new JPanel(){
         @Override
@@ -22,9 +32,11 @@ public class myFrame extends JFrame {
             super.paint(g);
             int num=0;
             for (shape s: shapes) {
-                System.out.println(s.getInfo());
+//                System.out.println(s.getInfo());
                 s.draw(g);
             }
+            if(shapes_temp != null)
+            shapes_temp.draw(g);
         }
     };
 
@@ -39,8 +51,19 @@ public class myFrame extends JFrame {
         this.add(jp1);
         this.add(jp2);
 
+        JSlider slider = new JSlider(0, 2);
+        slider.setMinorTickSpacing(1);
+        slider.setPaintLabels(true);
+        slider.setPaintTicks(true);
+        jp1.add(slider);
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                width = slider.getValue();
+            }
+        });
         //定义常量
-        String[] Buttons = new String[]{"Line", "Point", "Rectangle","Clear", "Triangle", "Curve","ColorChooser"};
+        String[] Buttons = new String[]{"Line", "Ellipse", "Rectangle","Circle", "ColorChoose", "Triangle", "Curve","Fill"};
 
         //初始化面板1
         jp1.setPreferredSize(new Dimension(200, 700));
@@ -61,7 +84,6 @@ public class myFrame extends JFrame {
         this.setVisible(true);
     }
     public void paint(Graphics g){
-        System.out.println("调用了JFrame的paint");
         super.paint(g);
     }
 
@@ -76,15 +98,30 @@ public class myFrame extends JFrame {
             p2.x = e.getX();
             p2.y = e.getY();
             if("Line".equals(name)){
-                shapes.add(new Line(p1, p2));
-                for (shape s:
-                        shapes
-                ) {
-                    System.out.println(s.getInfo());
-                }
+                shapes.add(new Line(p1, p2, frontcolor, width));
                 repaint();
                 System.out.println("shapes的大小为"+shapes.size());
             }
+            else if("Rectangle".equals(name)){
+                shapes.add(new Rectangle(p1,p2, frontcolor, width));
+                shapes_temp = null;
+                repaint();
+            }
+            else if("Circle".equals(name)){
+                shapes.add(new Circle(p1,p2, frontcolor, width));
+                shapes_temp = null;
+                repaint();
+            }
+            else if("Ellipse".equals(name)){
+                shapes.add(new Ellipse(p1,p2, frontcolor, width));
+                shapes_temp = null;
+                repaint();
+            }
+            else if("Text".equals(name)){
+                JTextField jtx = new JTextField();
+                jtx.setFont(new Font("楷体",Font.BOLD,16));    //修改字体样式
+            }
+
         }
 
         @Override
@@ -92,6 +129,10 @@ public class myFrame extends JFrame {
             if("".equals(e.getActionCommand())){
                 JButton jb = (JButton) e.getSource();
                 System.out.println("点击了空白！");
+            }
+            else if("ColorChoose".equals(e.getActionCommand())){
+                frontcolor = JColorChooser.showDialog(jp2, "colorchooser", Color.gray);
+
             }
             else{
                 System.out.println("点击了按钮！");
@@ -109,6 +150,14 @@ public class myFrame extends JFrame {
             System.out.println("按下了！");
             p1.x = e.getX();
             p1.y = e.getY();
+            if("Fill".equals(name)){
+                try {
+                    shapes_temp = new Fill(p1,Color.black);
+                } catch (AWTException awtException) {
+                    awtException.printStackTrace();
+                }
+                repaint();
+            }
         }
 
         @Override
@@ -122,16 +171,32 @@ public class myFrame extends JFrame {
         }
 
         @Override
-        public void mouseDragged(MouseEvent e) {
-//        if("Curve".equals(name)){
-//            p1.x = start_x;
-//            p1.y = start_y;
-//            p2.x = e.getX();
-//            p2.y = e.getY();
-//            shape s = new Curve(g, p1, p2);
-//            s.draw();
-//            myFrame.shapes.add(s);
-//        }
+        public void mouseDragged(MouseEvent e)
+        {
+            if("Curve".equals(name)){
+                p2.x = e.getX();
+                p2.y = e.getY();
+                shapes.add(new Line(p1, p2, frontcolor, width));
+                p1.x = p2.x;
+                p1.y = p2.y;
+                repaint();
+            }else if("Rectangle".equals(name)){
+                p2.x = e.getX();
+                p2.y = e.getY();
+                shapes_temp = new Rectangle(p1,p2, frontcolor, width);
+                repaint();
+            }else if("Circle".equals(name)){
+                p2.x = e.getX();
+                p2.y = e.getY();
+                shapes_temp = new Circle(p1,p2, frontcolor, width);
+                repaint();
+            }
+            else if("Ellipse".equals(name)){
+                p2.x = e.getX();
+                p2.y = e.getY();
+                shapes_temp = new Ellipse(p1,p2, frontcolor, width);
+                repaint();
+            }
         }
 
         @Override
